da0b738925659119659adb1db8191b6d95c33871
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-08 11:11:36 +0300

    Let + const

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-/* 
-Переменная
-Змінна
-Variable
-*/
+let box1 = 'Hello';
 
-/*
-Створення змінної
 
-ключове-слово ім'я-змінної
+const box2 = true;
 
-var -- deprecated
-
-let - CD-RW
-const - CD-R
-
-*/
-
-let box; // Оголошення змінної (Объявление переменной) (Variable Declaration)
-
-/* Оператор присвоєння = */
-
-box = 3; // Присвоєння (Присваивание) (Assign)
-console.log(box);
-
-let box2 = "Hello";
+console.log(box1);
 console.log(box2);
 
-box2 = 10;
-
-/* 
-
-Правила іменування
-
-*/
-
-let important variable = 0;
-
-/* 
-CamelCase
-*/
-
-let importantVariable = 0;
-
-let a = 6;
-
-// Ім'я змінної має пояснювати, що в ній зберігається
-
-let data; // bad practice
-let value; // bad practice
-
-let _ // lodash
-let $ // jquery
-
-let 123value; // Це заборонено!
+box1 = 10;
+// box2 = 15;
 
 
-let #value; // private field
-let _value; // private field
+const box;
+box = 5;
