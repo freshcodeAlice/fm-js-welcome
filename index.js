987dc6939f8de98dd6014868a28df5749b6e0519
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-08 10:40:59 +0300

    Data types

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log("Hello you");
-
-alert('Hello world'); // convention about '
+console.log(true); // boolean
+console.log("This is true"); // string
+console.log(50); // number
+console.log(undefined);
+console.log(null);
+console.log(5n); // BigInt
