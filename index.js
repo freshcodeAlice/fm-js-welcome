1d4d919043d7b2772e8e38ba89b953c3a323cf2a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-08 11:34:49 +0300

    +

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-let box;
+let num = 0.1 + 0.2;
 
-console.log(box);
+// console.log(num);
 
-
-box = 15;
-box = 20;
+let num1 = NaN; // Not-a-number
 
 
-const box2 = 15;
+let a = 1 + 2;
+let b = 2 - 1;
+let c = 2 * 2;
+let d = 4 / 2;
 
-box2 = 20;
 
-console.log(box2); // Не виконається через помилку на рядку 12
+let notAllowed = 5 / 0;
+console.log(notAllowed);
+let divTo0 = 4 / Infinity;
+console.log(divTo0);
+
+/* +
+Складати числа
+Зклеювати строки (конкатенація)
+*/
+
+let newLine = 'HEllo' + 'you';
+console.log(newLine);
+
+newLine = 5 + 'Hello';
+console.log(newLine);
+
+newLine = 1 + '1';
+console.log(newLine);
