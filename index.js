9941c1fd641f39c1038bdfef1536225b532b000a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-08 11:05:10 +0300

    Variable welcome

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
-console.log(true); // boolean
-console.log("This is true"); // string
-console.log(50); // number
-console.log(undefined);
-console.log(null);
-console.log(5n); // BigInt
+/* 
+Переменная
+Змінна
+Variable
+*/
+
+/*
+Створення змінної
+
+ключове-слово ім'я-змінної
+
+var -- deprecated
+
+let - CD-RW
+const - CD-R
+
+*/
+
+let box; // Оголошення змінної (Объявление переменной) (Variable Declaration)
+
+/* Оператор присвоєння = */
+
+box = 3; // Присвоєння (Присваивание) (Assign)
+console.log(box);
+
+let box2 = "Hello";
+console.log(box2);
+
+box2 = 10;
+
+/* 
+
+Правила іменування
+
+*/
+
+let important variable = 0;
+
+/* 
+CamelCase
+*/
+
+let importantVariable = 0;
+
+let a = 6;
+
+// Ім'я змінної має пояснювати, що в ній зберігається
+
+let data; // bad practice
+let value; // bad practice
+
+let _ // lodash
+let $ // jquery
+
+let 123value; // Це заборонено!
+
+
+let #value; // private field
+let _value; // private field
