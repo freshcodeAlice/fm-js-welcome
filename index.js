4c277d62eb1281d1a12a407816492d0634ddabe7
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-11 09:45:18 +0300

    Tasks resolve

--- index.js
+++ index.js
@@ -9,11 +9,46 @@
 
 */
 
+//1
+
+let value1 = Number(prompt('Введіть перше число')); // String
+let value2 = Number(prompt('Введіть друге число'));
 
 function getBigger(num1, num2) {
     if(num1 > num2) {
-        return num1;
+        return num1; 
     } else {
         return num2;
     }
+}
+
+let result = getBigger(value1, value2);
+console.log(result);
+
+
+//2
+
+function average(num1, num2) {
+    let avrg = (num1 + num2) / 2;
+    return avrg;
+}
+
+
+let res2 = average(value1, value2);
+console.log(res2);
+
+// 3
+
+function isEven(num) {
+    // if (num % 2 === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return num % 2 === 0 //скорочений результат
+
+
+    return !(num % 2) //дуже скорочений результат
+
 }
