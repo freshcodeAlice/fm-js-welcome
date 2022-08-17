a0c63476777c6dd5614fdddfd21f1219aade2c37
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-17 09:34:59 +0300

    Array welcome

--- index.js
+++ index.js
@@ -1,92 +1,16 @@
-const user = {
-    name: 'John',
-    lastName: 'Doe',
-    age: 20,
-    'favorite color': 'red',
-    movie: 'Blue moon',
-    avatar: 'http://....'
+let userTel1 = '38099423342334';
+let userTel2 = '38023423423234';
+let userTel3 = '43243234234234';
+
+const userTels = {
+    0: '38099423342334',
+    1: '38023423423234',
+    2: '43243234234234',
+    7: '23423423423423'
 }
 
-
-// for ... in
-
-// НЕ ЦЕЙ for(;;)
-
-for (let key in user) {
-    console.log(`${key} --- ${user[key]}`); // key --- value
-}
+userTels[0] // '38099423342334',
 
 
-/*
-
-Написати функцію, яка приймає об'єкт і виводить всі значення всіх властивостей, оформлені у *...*
-
-obj = {
-    key1: value2
-}
-
-/// "key1": *value2*
-
-
-
-*/
-
-
-function stylingConsole(obj) {
-    for(let key in obj) {
-        console.log(`"${key}": *${obj[key]}*`)
-    }
-}
-
-
-/*
-
-Task2: 
-
-НАписати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів
-
-*/
-
-const departmentSalary = {
-    HR: 20000,
-    Development: 30000,
-    Drive: 200,
-    Cleaning: null
-}
-
-
-function sumSalary (salaryObj) { 
-
-    let result = 0;
-
-    for (let key in salaryObj) {
-        if( typeof salaryObj[key] === 'number') {
-            result = result + salaryObj[key];
-        }
-    }
-    return result;
-
-}
-
-console.log(sumSalary(departmentSalary));
-
-
-
-
-/*
-
-Задачка з *: написати функцію, яка перевіряє об'єкт на пустоту (тобто відсутність властивостей чи методів). Повертає Boolean
-
-const a ={
-    key: value
-}
-
-isEmpty(a) // false
-
-const b = {
-
-}
-
-isEmpty(b) // true
-
-*/
+const array = [7,3,5,2];
+array[2];
