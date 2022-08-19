9e9aaf6c658df3bff41b98b7312c181b42191d39
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-19 12:04:54 +0300

    Random cat task

--- index.js
+++ index.js
@@ -1,13 +1,117 @@
-// reduce
-// зводить якусь кількість елементів до одного результуючого значення
+// sort, filter, map, forEach
 
-const array = [2, 3, 1, 2, 34, 21];
+const catArray = [
+    {
+        name: 'Murzik',
+        color: 'black',
+        weight: 3,
+        age: 2
+    },
+    {
+        name: 'Murka',
+        color: 'grey',
+        weight: 5,
+        age: 3
+    },
+    {
+        name: 'Barsik',
+        color: 'red',
+        weight: 2,
+        age: 1
+    },
+    {
+        name: 'Cookie',
+        color: 'brown',
+        weight: 5,
+        age: 2
+    },
+    {
+        name: 'Stus',
+        color: 'grey',
+        weight: 2,
+        age: 5
+    },
+    {
+        name: 'Fagot',
+        color: 'black',
+        weight: 5,
+        age: 3
+    },
+    {
+        name: 'Kit',
+        color: 'pink',
+        weight: 2,
+        age: 1
+    },
 
-function reducer (accumulator, elem) {
-    return accumulator + elem
-}
+];
 
 
-const sum = array.reduce(reducer, 0);
+// 1
 
-// array.reduce((acc, el) => acc+el, 0);
+/*
+
+Відсортувати котів за вагою на збільшення
+*/
+
+
+catArray.sort(function (cat1, cat2) {
+   if(cat1.weight < cat2.weight) {
+    return -1;
+   } else {
+    return 1;
+   }
+})
+
+/*
+-1 -> cat1, cat2
+0 -> всі на місці
+1 -> cat2, cat1
+
+*/
+
+
+/*
+відсортувати котів від найстаршого до наймолодшого 
+
+*/
+
+catArray.sort(function(cat1, cat2) {
+    return cat2.age - cat1.age;
+});
+
+
+/*
+
+відсіяти всіх котів окрім чорних
+
+*/
+
+const blackCatArray = catArray.filter(function(cat) {
+    return cat.color === 'black'
+});
+
+/*
+
+створити новий масив, який складається з імен котів
+
+*/
+
+const catNames = catArray.map(function (cat) {
+    return cat.name;
+})
+
+/*
+
+Додати котам поле isMale
+true - кіт,
+false - кішка
+
+*/
+
+
+/* Рандомне число - або 0, або 1
+Math.round(Math.random())
+Boolean(0) - false
+Boolean(1) - true
+*/
