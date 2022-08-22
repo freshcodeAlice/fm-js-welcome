37f7c16a1c52dd74156e78d4a68564f84df22ad3
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-22 10:47:32 +0300

    __proto__ literal

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+/*
+
 function MyArray(){
     this.length = 0;
 
@@ -25,3 +28,35 @@ function MyArray(){
 
 
 const arr = new MyArray();
+
+
+
+
+
+*/
+
+
+const cat = {
+    name: 'Barsik',
+    color: 'red',
+    age: 1
+};
+
+const cat2 = {
+    name: 'Murzik',
+    color: 'black',
+    age: 5
+}
+
+const catMethods = {
+    run: function() {
+        console.log('Cat is running');
+    },
+    meow: function () {
+        console.log('Meow!');
+    }
+}
+
+
+cat.__proto__ = catMethods;
+cat2.__proto__ = catMethods;
