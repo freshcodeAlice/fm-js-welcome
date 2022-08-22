3f8b12a266ed11db19aefe88e6366e0aeb31752b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-22 12:03:23 +0300

    Accumulator task

--- index.js
+++ index.js
@@ -65,5 +65,43 @@ function LadderProto () {
 
 Ladder.prototype = new LadderProto();
 
-// Перепишіть методи таким чином, щоб можливо було використання чейнінгу, тобто щоб можна було об'єднати виклик методів у ланцюжок:
+const lad = new Ladder();
+lad.up();
 
+// Перепишіть методи таким чином, щоб можливо було використання чейнінгу, тобто щоб можна було об'єднати виклик методів у ланцюжок
+
+
+/*
+
+Зробити функцію-конструктор об'єкту Accumulator, який приймає певне початкове значення при створенні
+
+new Accumulator(startingValue)
+має властивість
+value
+
+має метод read(), який за допомогою prompt приймає від користувача число та додає його до value, повертає значення поточного value
+(Перевірка на число)
+
+*/
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+}
+
+
+function AccumulatorProto() {
+    this.read = function() {
+        const userValue = Number(prompt('Введіть число'));
+        if(Number.isNaN(userValue)) {
+            console.log('Це не число');
+        } else {
+            this.value = userValue;
+        }
+
+    }
+}
+
+Accumulator.prototype = new AccumulatorProto();
+
+/* Таска самостійна: передбачити перевірку початкового значення при створенні аккумулятору І при додаванні нового значення методом read. При цьому не дублювати код
+*/
