bca98915a0a36ecf1ac68c669e8f76033aa8db97
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-22 11:45:22 +0300

    Ladder task resolve

--- index.js
+++ index.js
@@ -26,3 +26,44 @@ function MyProtoArray() {
 
 MyArray.prototype = new MyProtoArray();
 
+
+
+/*
+
+Задача: створити функцію-конструктор для сходів (Ladder).
+Об'єкт має властивість 
+currentStair // сходинка, на якій ми зараз знаходимось. Початково = 0
+
+Має методи:
+up() - піднімає на сходинку вище
+down() - опускає на сходинку нижче
+showStair() - показує, на якій сходинці ми зараз знаходимось
+
+
+*/
+
+
+function Ladder() {
+    this.currentStair = 0;
+}
+
+function LadderProto () {
+    this.up = function() {
+        this.currentStair++;
+        return this;
+    }
+
+    this.down = function() {
+        this.currentStair--;
+        return this;
+    }
+
+    this.showStair = function() {
+        return this.currentStair;
+    }
+}
+
+Ladder.prototype = new LadderProto();
+
+// Перепишіть методи таким чином, щоб можливо було використання чейнінгу, тобто щоб можна було об'єднати виклик методів у ланцюжок:
+
