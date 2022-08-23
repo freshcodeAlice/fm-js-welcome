dfa218d8a76278806d0aabb9edbfda0fb3242fa0
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-23 11:16:08 +0300

    Rest-n-Spred

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // rest operator - ...
+// .., .., .., .., -> []
 
 function sum(a,b, ...arrayOfRestArguments) {
     console.log(arrayOfRestArguments);
@@ -13,8 +14,24 @@ sum(3, 2, 5, 2345, 123, 12);
 
 const arrowSum = (...restArray) => restArray.reduce((accum, current) => accum+current)
 
-/*
 
-Напишіть варіант цієї функції через метод reduce
+// spred - ...
+// [] -> .., .., .., ..,
 
+const numbers = [1, 2, 32, 112, 32, 45];
+sum(...numbers);
+
+
+/* Швидкий спосіб копіювання масиву */
+const newNumber = [...numbers]
+
+
+/* 
+Склейте два масива в один, не використовуючи метод concat
 */
+
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [10, 11, 12, 13, 14, 15]; // [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15];
+
+const concatArray = [...arr1, ...arr2];
