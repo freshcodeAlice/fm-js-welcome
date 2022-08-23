ad2361fc7c81ed545586989004a695d18b935beb
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-23 10:47:34 +0300

    Rest operator

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
 'use strict'
 
-function sum(a,b) {
+// rest operator - ...
+
+function sum(a,b, ...arrayOfRestArguments) {
+    console.log(arrayOfRestArguments);
     return a+b;
 }
 
-const arrow = (a, b) => a + b // a+b === {return a+b}
+sum(3, 4); // 7
+sum(1, 2, 3); //3
+sum(3, 2, 5, 2345, 123, 12);
 
-arrow();
-
-const pow = x => x*x //якщо 1 аргумент, то () можна опустити
-
-const SuperFunc = () => { // Arrow Function не може бути функцією-конструктором!
-    this.value = 'hello'
-}
-
-
-function SuperFun () {
-    // this = {}
-    this.value = 'hi'
-}
-
-
-const t1 = function () {
-    console.log(arguments);
-}
-
-const t2 = () => {
-    console.log(arguments); // Arrow Function не має arguments
-}
-
-
-console.dir(t1);
-
-console.dir(t2);
-
-
-t1(1, 2, 3);
-
-t2(5, 6, 7);
-
-// Функція суми всіх переданих аргументів
-
-function sum() {
-    let result = 0;
-    for (let i = 0; i < arguments.length; i++) {
-      result += arguments[i]
-    }
-}
-
+const arrowSum = (...restArray) => restArray.reduce((accum, current) => accum+current)
 
 /*
 
-1. Стрілочна функція не має власного контексту (її this буде вказувати на середовище її виконання)
-2. Стрілочна функція не може бути функцією-конструктором.
-3. Стрілочна функція не має колекції arguments
-
+Напишіть варіант цієї функції через метод reduce
 
 */
