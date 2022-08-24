6d88ca7c7fd62e03ba4b692d619c334a54995d14
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-24 09:54:03 +0300

    Primitive methods

--- index.js
+++ index.js
@@ -1,91 +1,52 @@
-/*
-Вивести на консоль таблицю множення від 1 до 5 у вигляді:
-1х1 = 1
-1х2 = 2
-.....
-2х1 = 2
-2х2 = 4
-.....
-5х1=5
-5х2=10
-5х3
-5х4
-5х5
+const bool = true; // bool - це просто примітив логічного типу
 
+bool.toString(); /*
+ Створюється об'єкт-обгортка Boolean:
+ obj = new Boolean(true);
+obj.toString();
+отримуємо результат, і об'єкт-обгортка знищується
 */
 
-for (let i=1; i <= 5; i++) { // 2
-    for(let j=1; j<= 5; j++) { // 1...2...3...4...5
-       // console.log(`${i} x ${j} = ${i*j}`);
-    }
-}
+/* Number */
+
+const num = 23.234234234234;
+num.toFixed(0);
+
+Number.parseInt('1001', 2);
+
+Number.parseFloat('123.21');
 
 
-const number = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+/* String */
 
-/* 
-Напишіть функцію, яка знайде суму масива number
-*/
-
-function sumArray (array) {
-    // array = [1, 2, 3, 4, 5, 6];
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        for(let j=0; j <= 2; j++) {
-             sum += array[i][j]
-        }
-    }
-    return sum
-}
-
-sumArray(number)
-
-
-/*
-
-Намалювати в консолі таку фігуру:
-
-*
-**
-***
-****
-*****
-******
-*******
-********
-
-*/
-
-
-function drowFigure(dimension) {
-    let figure = '';
-
-    for (let i = 1; i <= dimension; i++) {
-      for( let j = 0; j < i; j++) {
-        figure+='*'
-      }    
-      figure+='\n'
-    }
-    
-    console.log(figure)
-}
-
-
-drowFigure(8);
-
-
-/*
-
-Написати функцію, яка приймає розмір фігури в аргументи і виводить ось таку фігуру:
-
-**************
-* *          *
-*   *        *
-*     *      *
-*       *    *
-*         *  *
-*           **
-**************
-
-
-*/
+const str = 'igelschnäuzchen';
+str;
+//'igelschnäuzchen'
+str.charAt(5);
+//'c'
+str[5];
+//'c'
+str.charCodeAt(5)
+//99
+str.concat(' hello')
+//'igelschnäuzchen hello'
+str;
+//'igelschnäuzchen'
+str.includes('igel')
+//true
+str.includes('cat')
+//false
+str.indexOf('c')
+//5
+str.repeat(5)
+//'igelschnäuzchenigelschnäuzchenigelschnäuzchenigelschnäuzchenigelschnäuzchen'
+str.replace('igel', 'cat');
+//'catschnäuzchen'
+'       fdsfdf         '.trim()
+//'fdsfdf'
+'hi hi hi'.split(' ');
+//(3) ['hi', 'hi', 'hi']0: "hi"1: "hi"2: "hi"length: 3[[Prototype]]: Array(0)
+'ewerwer*werfwerW*RWRWersdfdsf*gfd'.split('*')
+//(4) ['ewerwer', 'werfwerW', 'RWRWersdfdsf', 'gfd']0: "ewerwer"1: "werfwerW"2: "RWRWersdfdsf"3: "gfd"length: 4[[Prototype]]: Array(0)
+'rewrerweerwerwer'.split('');
+//(16) ['r', 'e', 'w', 'r', 'e', 'r', 'w', 'e', 'e', 'r', 'w', 'e', 'r', 'w', 'e', 'r']
