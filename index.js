4f88cfb58696bdd383972d9088ff35855c8435fc
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-24 11:01:23 +0300

    String methods tasks resolve

--- index.js
+++ index.js
@@ -1,52 +1,90 @@
-const bool = true; // bool - це просто примітив логічного типу
+/* 
+Задачка 1: Написати функцію, яка робить кожне слово у переданому рядку з великої літери
 
-bool.toString(); /*
- Створюється об'єкт-обгортка Boolean:
- obj = new Boolean(true);
-obj.toString();
-отримуємо результат, і об'єкт-обгортка знищується
 */
 
-/* Number */
+function eachWordInUpper(str) {
+  const wordArray = str.split(' ');
+  const resultWordArray = [];
+  for (let i = 0; i < wordArray.length; i++) {
+      // wordArray[i][0] = wordArray[i].charAt(0).toUpperCase();   
+      let word = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1);
+      resultWordArray.push(word);
+  }
+  return resultWordArray.join(' ');
+}
 
-const num = 23.234234234234;
-num.toFixed(0);
 
-Number.parseInt('1001', 2);
+/*
 
-Number.parseFloat('123.21');
+Таска:
+Написати функцію truncate, яка приймає в якості аргументів рядок (str) і довжину (strLength).
+Якщо довжина > strLength, то врізати рядок до цієї довжини і в кінці додати "..."
+Якщо рядок менший, нічого не робити
+
+truncate('hello', 2) // 'he...'
+truncate('hello', 10) // 'hello'
+*/
+
+function truncate(str, strLength) {
+  if (str.length <= strLength) {
+    return str;
+  }
+
+  const newStr = str.slice(0, strLength);
+  return newStr+'...'
+
+}
 
 
-/* String */
+/*
 
-const str = 'igelschnäuzchen';
-str;
-//'igelschnäuzchen'
-str.charAt(5);
-//'c'
-str[5];
-//'c'
-str.charCodeAt(5)
-//99
-str.concat(' hello')
-//'igelschnäuzchen hello'
-str;
-//'igelschnäuzchen'
-str.includes('igel')
-//true
-str.includes('cat')
-//false
-str.indexOf('c')
-//5
-str.repeat(5)
-//'igelschnäuzchenigelschnäuzchenigelschnäuzchenigelschnäuzchenigelschnäuzchen'
-str.replace('igel', 'cat');
-//'catschnäuzchen'
-'       fdsfdf         '.trim()
-//'fdsfdf'
-'hi hi hi'.split(' ');
-//(3) ['hi', 'hi', 'hi']0: "hi"1: "hi"2: "hi"length: 3[[Prototype]]: Array(0)
-'ewerwer*werfwerW*RWRWersdfdsf*gfd'.split('*')
-//(4) ['ewerwer', 'werfwerW', 'RWRWersdfdsf', 'gfd']0: "ewerwer"1: "werfwerW"2: "RWRWersdfdsf"3: "gfd"length: 4[[Prototype]]: Array(0)
-'rewrerweerwerwer'.split('');
-//(16) ['r', 'e', 'w', 'r', 'e', 'r', 'w', 'e', 'e', 'r', 'w', 'e', 'r', 'w', 'e', 'r']
+Написати функцію checkSpam, яка повертає true, якщо переданий рядок містить слова 'xxx' або 'viagra', інакше false
+
+checkSpam('buy ViAgRa now') // true
+checkSpam('free xxxxxx' ) // true
+checkSpam('innocent rabbit') //false
+
+
+*/
+
+//variant 1
+
+function checkSpam(str) {
+  const lowStr = str.toLowerCase();
+  return lowStr.includes('viagra') || lowStr.includes('xxx')
+}
+
+//variant 2
+
+
+function checkSpam2(str) {
+  const spamArray= ['viagra', 'xxx', 'drags'];
+
+  let flag = false;
+
+  for (let i = 0; i < spamArray.length; i++) {
+    if(str.toLowerCase().includes(spamArray[i])) {
+      flag = true
+    }
+  }
+  return flag
+}
+
+
+
+
+/*
+
+Задачка: написати функцію, яка перевіряє, чи є переданий рядок Паліндромом. не зважаючи на регістр
+Паліндром - це коли рядок з обоих сторін читається однаково.
+
+Anna - паліндром
+Mama - не паліндром
+Namman - паліндром
+*/
+
+
+function palindrom(str) {
+  return str.toLowerCase().split('').reverse().join('') === str.toLowerCase()
+}
