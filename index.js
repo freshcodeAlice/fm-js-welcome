ebc7b2a71def1c834ec93f584d14fec1ae2c6933
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-29 11:31:05 +0300

    extends

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
-/*
-
-Напишіть клас RangeValidator
-дві властивості:
-from, to
-
-Обидва числа
-From не може бути більше ніж to
-
-Завдання: реалізувати сеттери для обох властивостей
-Геттер getRange, який має повертати масив цілих чисел цього діапазону
+/* User */
 
 
-from = 2; to = 5
-getRange() // [2, 3, 4, 5]
-
-
-Задачка з *:
-реалізувати метод validate, який приймає число і повертає true, якщо це число входить у діапазон, false якщо ні
-
- */
-
-class RangeValidator {
-  constructor(from, to) {
-    this.to = to;
-    this.from = from; // this._from
+class User {
+  constructor(name, surname, age) {
+    this.name= name;
+    this.surname = surname;
+    this.age = age;
   }
 
-  set from (value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('From must be a number');
-    } 
-    if (value > this._to) {
-      throw new RangeError('From must be a larger than to');
-    }
-    this._from = value;
-  }
-
-  set to(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('To must be a number');
-    }
-    this._to = value;
-  }
-
-  get to(){
-    return this._to;
-  }
-
-  get from() {
-    return this._from;
-  }
-
-
-  get getRange() {
-    const arr = [];
-    for (let i = this._from; i < this._to; i++) {
-      arr.push(i);
-    }
-    return arr;
-  }
-
-
-  validate(num) {
-    return this.getRange.includes(num);
+  getFullName() {
+    return `${this.name} ${this.surname}`
   }
 }
+
+
+class Moderator extends User {
+  constructor(name, surname, age) {
+    super(name, surname, age);
+  }
+
+  createPost(header, text) {
+
+  }
+
+  deletePost(index) {
+
+  }
+}
+
+
+class Admin extends Moderator {
+  constructor(name, surname, age, isAdmin) {
+    super(name, surname, age);
+    this.isAdmin = true;
+  }
+
+  setModerator(userId) {
+
+  }
+
+  deleteModerator() {
+
+  }
+}
+
+
+/*
+Класс Owner, який буде вміти все те, що і Адмін, плюс в нього буде поле доступу до магазину ShopAccess 
+Метод getAnnualReport
+
+
+*/
