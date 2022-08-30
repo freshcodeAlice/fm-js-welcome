3d389bd9f410c8c2fb94afd7665643be4ff1400d
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-30 09:47:06 +0300

    Incapsulation

--- index.js
+++ index.js
@@ -1,50 +1,81 @@
-class Squirrel {
-  constructor(name, color) {
-    // create {} --> this
-    this.name = name;
-    this.color = color;
+class Figure {
+  constructor(sideQuantity) {
+    this.sideQuantity = sideQuantity
   }
 
-  climb() {
-    return `${this.name} is climbing!`
+  getArea() {
+
   }
 }
 
-class FlyingSquirrel extends Squirrel {
-    constructor(name, color, maxDistance) {
-      super(name, color); // create Squirrel {} ---> this
-      this.maxDistance = maxDistance;
-    }
 
-    fly(distance) {
-      if(distance < this.maxDistance) {
-        return `${this.name} can fly so far`
-      }
-    }
-}
-
-
-class FairySquirrell extends FlyingSquirrel {
-  constructor(name, color, maxDistance, singArray) {
-    super(name, color, maxDistance);
-    this.singArray = singArray;
+class Triangle extends Figure {
+  constructor(a, b, angle) {
+    super(3);
+    this.a = a;
+    this.b = b;
+    this.angle = angle;
   }
 
-  singSong(song) {
-    if(this.singArray.includes(song)) {
-      return `${this.name} can song it!`
+  get a() {
+    return this._a;
+  }
+
+  set a(value) {
+    if(value < 0) {
+      throw new RangeError('Side cannot be less than 0');
     }
+    this._a = value;
+  }
+
+  get b() {
+    return this._b;
   }
   
-  fly() {
-    console.log('Our fairy Squirrell can fly unlimited');
+  set b(value) {
+    if(value < 0) {
+      throw new RangeError('Side cannot be less than 0');
+    }
+    this._b = value;
+  }
+
+  getArea() {
+    return this.a * this.b * Math.sin(this.angle);
   }
 }
 
-
-class SuperSquirrell extends FairySquirrell {
-  constructor(name, color, maxDistance, singArray) {
-    super(name, color, maxDistance, singArray);
+class Square extends Figure {
+  constructor(a) {
+    super(4);
+    this._a = a;
   }
 
+  get a() {
+    return this._a;
+  }
+
+  set a(value) {
+    if(value < 0) {
+      throw new RangeError('Side cannot be less than 0');
+    }
+    this._a = value;
+  }
+
+  getArea() {
+    return this.a*this.a;
+  }
+}
+
+/* Написати аналогічний клас Circle, який буде мати метод для обчислення площі та буде інкапсулювати дані (аксессори) */
+
+
+class Circle extends Figure {
+  constructor(r) {
+    super(1); 
+    this.r = r;
+  }
+
+  getArea () {
+    return Math.PI*this.r*this.r;
+  }
 }
