5bd6cf43c732c2ec8b03c656992e958238a1d1e4
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-31 11:07:40 +0300

    Symbol welcome

--- index.js
+++ index.js
@@ -1,88 +1,21 @@
-/*
-Є ферма.
-На фермі в нас є свійські тварини (корови, кози).
-Є домашні тварини (кошки і собаки).
-
-В кожної тварини є діти. В кожного з дітей може бути певна кількість дітей.
-
-Реалізувати класи всіх тварин і додати тваринам рандомну кількість дітей.
-
-Реалізувати функцію, яка підрахує кількість тварин на фермі.
+// Symbol
 
 
-створити масив
-зробити Н кількість ітерацій
-на кожній ітерації створювати нову тварину, їй теж створити кілька дітей і додавати її в масив
-
-const ferma = [cats, dogs, cows]
-
-*/
+const bl = true; // false - boolean
+const b1 = new Boolean({});
 
 
-class Animal {
-  constructor(){
-    this.childs = null;
-  }
+const str = 'hello';
+const str2 = new String('hello'); 
 
+
+const mySymbol = Symbol(); // без new!!
+
+// const symb2 = Symbol('My first symbol'); // label for human
+
+
+const obj = {
+  test: 1,
+  [mySymbol]: 123,
+  abradabra: '2123'
 }
-
-
-class Cat extends Animal{
-  constructor(){
-    super();
-  }
-}
-
-class Dog extends Animal{
-  constructor(){
-    super();
-  }
-}
-
-class Cow extends Animal{
-  constructor(){
-    super();
-  }
-}
-
-class Goat extends Animal{
-  constructor(){
-    super();
-  }
-}
-
-
-/**
- * 
- * @param {Animal (or Cat, Dog, Cow, Goat)} Type 
- * @param {number} quantity 
- */
-
-
-function createFamily(Type, quantity) {
-    const animals = new Array(quantity).fill(new Type());
-    animals.forEach(animal => {
-      const childs = new Array(quantity).fill(new Type());
-      animal.childs = childs;
-    })
-    return animals;
-}
-
-
-/*   Функція підрахунку кількості дітей */
-
-/**
- * 
- * @param {Array of Animals} farm 
- */
-
-function countFarm(farm) {
-    return farm.reduce((accum, item)=> {
-        if(item.childs) {
-          return accum + 1 + item.childs.length
-        }
-  },0)
-}
-
-
-const farm = [...createFamily(Cat, 3), ...createFamily(Dog, 2), ...createFamily(Cow, 5), ...createFamily(Goat, 1)];
