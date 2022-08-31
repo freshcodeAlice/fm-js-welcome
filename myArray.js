0d5e0e72a52cfc02524ef982d8ef2431553eb8d5
freshcodeAlice <freshjs.ofte@gmail.com>
2022-08-31 10:16:17 +0300

    MyArray in class

--- myArray.js
+++ myArray.js
@@ -1,27 +1,43 @@
-function MyArray(){
-    this.length = 0;
-  
-}
+class MyArray {
+    constructor() {
+        this.length = 0;
+    }
 
-function MyProtoArray() {
-    this.push = function() {
-        for (let i = 0; i < arguments.length; i++) {
-            this[this.length] = arguments[i];
+    static isMyArray(obj) {
+        return obj instanceof MyArray; 
+    }
+
+    push(...rest) {
+        for (let i = 0; i < rest.length; i++) {
+            this[this.length] = rest[i];
             this.length++;
         }
-    },
-    this.pop = function(value) {
+    }
+
+    pop () {
         let lastItem = this[this.length - 1];
         delete this[this.length - 1];
         this.length--;
         return lastItem;
-    },
-    this.forEach = function(fn) {
+    }
+
+    forEach (fn) {
         for (let i = 0; i < this.length; i++) {
             fn(this[i]);
         }
     }
+
+    map(fn) {
+        const newArr = new MyArray();
+        for (let i = 0; i < this.length; i++) {
+            newArr.push(fn(this[i], i, this));
+        }
+        return newArr;
+    }
+  
 }
 
+/* 
+Напишіть реалізацію методу map()
 
-MyArray.prototype = new MyProtoArray();
+*/
