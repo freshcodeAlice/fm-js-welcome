cadf1b5362ce4836d5ab416adf98b5c92eb4204b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-06 11:27:51 +0300

    Stack

--- index.js
+++ index.js
@@ -1,69 +1,36 @@
-const arr = [2, 4, 1, 3, 7, 3, 2, 1, 3, 5, 2, 8, 2, 4, 1];
+/* Stack */
 
-function linearSearch(array, value) { // лінійна складність алгоритму
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === value) {
-            return i;
-        }        
+class Stack {
+    constructor (maxSize) {
+        this._maxSize = maxSize;
+        this._size = 0;
     }
-    return -1;
-}
 
-
-//linearSearch(arr, 9);
-
-
-function multyTable(limit) { //квадратична (парабола)
-    const table = [];
-    for (let i = 1; i <= limit; i++) {
-        for (let j = 1; j <= limit; j++) {
-            const number = i*j;
-            table.push(`${i}*${j} = ${number}`);
-        }        
+    get isEmpty() {
+        return this._size === 0;
     }
-    return table;
-}
 
-// multyTable(9)
+    get size(){
+        return this._size;
+    }
 
-
-const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-
-
-function binarySearch(array, whatToFind) { // логаріфмічна складність
-    let start = 0;
-    let end = array.length - 1;
-    let middle = Math.round(start+end/2);
-    while(true) {
-        debugger;
-        if (array[middle] === whatToFind) {
-            return middle;
+    push(value) {
+        if(this.size >= this._maxSize) {
+            throw new Error('Stack overflow');
         }
-        if(array[middle] < whatToFind) {
-            start = middle;
-            middle = Math.ceil((start+end)/2);
-        } else {
-            end = middle;
-            middle = Math.ceil((start+end)/2);
-        }
+        this[`_${this.size}`] = value;
+        this._size++;
+        return this._size;
     }
-}
 
-/*
-Переписати бінарний пошук з циклу на рекурсію
+    pop() {
+        const lastItem = this[`_${this.size-1}`];
+        delete this[`_${this.size-1}`];
+        this._size--;
+        return lastItem;
+    }
 
-*/
-
-function binarySearchRecursive(array, whatToFind) {
-    const middle = Math.floor(array.length/2);
-
-    if (array[middle] === whatToFind) {
-        return middle;
-    } else if (array[middle] > whatToFind) {
-        return binarySearchRecursive(array.slice(0, middle), whatToFind);
-    } else if (array[middle] < whatToFind) {
-        return middle + 1 + binarySearchRecursive(array.slice(middle+1, array.length), whatToFind);
-    } else {
-        return -Infinity;
+    pick() {
+        return this[`_${this.size-1}`];
     }
 }
